4bd7eb4dfc8edfd2bf23ef33dc29620191ac0dfe
Rafadinho <rafa85jogo@gmail.com>
2018-09-09 17:09:43 -0600

    agregando clase 4 funciones

---
+++ clase4.js
@@ -0,0 +1,17 @@
+//las funciones son pedazo
+//de codigo reutilizables
+
+var nombre = "Rafadinho"
+var edad = 22;
+
+//definir Funciones
+//function es una palabra reservada del lenguaje
+function imprimirEdad(n, e)
+{
+  console.log(n + " tiene " + e + " años ")
+}
+//llamando a la funcion
+// y pasandole parametros
+imprimirEdad(nombre, edad)
+imprimirEdad("Jery", 23)
+imprimirEdad("Helen", 25)
